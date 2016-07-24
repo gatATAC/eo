8f9a423e2578712cfd3b1c1e234ed9517a3af38b
Txinto Vaz <txinto@gatatac.org>
2016-07-24 12:56:42 +0100

    Fix default roles

--- db/migrate/20160616002703_add_roles.rb
+++ db/migrate/20160616002703_add_roles.rb
@@ -9,14 +9,6 @@ class AddRoles < ActiveRecord::Migration
     add_column :project_memberships, :role_id, :integer
 
     add_index :project_memberships, [:role_id]
-    
-    Role.create :name => "Systems Engineer", :abbrev => "sys"    
-    Role.create :name => "Project Manager", :abbrev => "pm"
-    Role.create :name => "Principal Investigator", :abbrev => "pi"
-    Role.create :name => "Optical Engineer", :abbrev => "opt"    
-    Role.create :name => "Mechanical Engineer", :abbrev => "mech"
-    Role.create :name => "Electronics Engineer", :abbrev => "hw"
-    Role.create :name => "Software Engineer", :abbrev => "sw"
   end
 
   def self.down
--- db/migrate/20160616003210_add_role_abbrev.rb
+++ db/migrate/20160616003210_add_role_abbrev.rb
@@ -1,6 +1,16 @@
 class AddRoleAbbrev < ActiveRecord::Migration
   def self.up
     add_column :roles, :abbrev, :string
+    
+    
+    Role.create :name => "Systems Engineer", :abbrev => "sys"    
+    Role.create :name => "Project Manager", :abbrev => "pm"
+    Role.create :name => "Principal Investigator", :abbrev => "pi"
+    Role.create :name => "Optical Engineer", :abbrev => "opt"    
+    Role.create :name => "Mechanical Engineer", :abbrev => "mech"
+    Role.create :name => "Electronics Engineer", :abbrev => "hw"
+    Role.create :name => "Software Engineer", :abbrev => "sw"
+    
   end
 
   def self.down
