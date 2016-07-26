ab8724501ba0a5ce998b153f0ac381c749e489a5
Txinto Vaz <txinto@gatatac.org>
2016-07-26 16:52:01 +0100

    Some improvements in issue creation, not yet functional

--- app/controllers/project_rms_controller.rb
+++ app/controllers/project_rms_controller.rb
@@ -4,11 +4,19 @@ class ProjectRmsController < ApplicationController
 
   auto_actions :all
   
+  web_method :sync_issues
+  
   before_action :reload_issues, :only => :show
 
   def reload_issues
-    @project_rms = find_instance
+    @project_rm = find_instance
     find_instance.reload_issues
+  end
+  
+  def sync_issues
+    @project_rm = find_instance
+    @project_rm.sync_issues
+    redirect_to this
   end
 
 end
--- app/models/project_rm.rb
+++ app/models/project_rm.rb
@@ -44,22 +44,24 @@ class ProjectRm < ActiveRecord::Base
           end
           issue_rm.author = issue.author.id
           issue_rm.tracker = issue.tracker.id
-          issue.custom_fields.each{|f|
-            if f.name=="eosys" then
-              if (f.value=="1") then
-                issue_rm.eosys=true
-              else
-                issue_rm.eosys=false
+          if (issue.custom_fields) then
+            issue.custom_fields.each{|f|
+              if f.name=="eosys" then
+                if (f.value=="1") then
+                  issue_rm.eosys=true
+                else
+                  issue_rm.eosys=false
+                end
               end
-            end
-            if f.name=="eosysid" then
-              if (f.value) then
-                sysid=Integer(f.value)
-                sys=System.find_by_id(sysid)
-                issue_rm.system=sys
+              if f.name=="eosysid" then
+                if (f.value) then
+                  sysid=Integer(f.value)
+                  sys=System.find_by_id(sysid)
+                  issue_rm.system=sys
+                end
               end
-            end
-          }
+            }
+          end
           issue_rm.save
         end
       end
@@ -69,7 +71,240 @@ class ProjectRm < ActiveRecord::Base
       irm.delete
     }
   end  
+
+  def create_sample_issue
+    all_trackers=RedmineRest::Models::Tracker.all
+    tr=nil
+    all_trackers.each {|t|
+      print "Tracker #{t.name} item:"+t.id.to_s+"\n"
+      if t.name=="Feature" then
+        tr=t
+        print ("***** ESTE\n")
+      end
+    }
+    all_projects=RedmineRest::Models::Project.all
+    pr=nil
+    all_projects.each {|t|
+      print "Project #{t.name} item:"+t.id.to_s+"\n"
+      if t.id.to_i == self.rm_project then
+        pr=t
+        print ("***** ESTE\n")
+      end
+    }
+    
+    all_users=RedmineRest::Models::User.all
+    us=nil
+    all_users.each {|t|
+      print "User #{t.login} item:"+t.id.to_s+"\n"
+      if t.login=="txinto" then
+        us=t
+        print ("***** ESTE\n")
+      end
+    }
+    
+    #Create an Issue
+    print "Creamos una issue\n"
+    i=Issue.new
+    i.project=pr
+    i.tracker=tr
+    i.subject="Test issue created from Eo"
+    i.author_id=us
+    i.status_id=1
+    i.priority_id=1
+    i.description="This is a test"
+    i.custom_fields = []
+    i.custom_fields << {:name => 'eosys', :value => true, :id => 10}
+    i.custom_fields << {:name => 'eosysid', :value => "1", :id => 11}
+    print i.custom_fields.to_s
+    done=i.save
+    print "Hecho: "+done.to_s+"\n"
+    print "Issue:"+i.to_s+"\n"
+    print "Tracker:"+i.tracker.id.to_s+"\n"
+    print "Project:"+i.project.id.to_s+"\n"    
+  end
   
+  def sync_issues
+    self.issue_rms.each{|i|
+      rm_issue=Issue.find_by_id(i.rm_ident)
+      if (rm_issue) then
+        rm_issue.destroy
+      end
+      i.destroy
+    }
+    all_projects=RedmineRest::Models::Project.all
+    pr=nil
+    all_projects.each {|t|
+      print "Project #{t.name} item:"+t.id.to_s+"\n"
+      if t.id.to_i == self.rm_project then
+        pr=t
+        print ("***** ESTE\n")
+      end
+    }
+    existing_issues=self.issue_rms.clone
+    systosync = []
+    self.project.systems.each{|sys|
+      parentatomic=false
+      if (sys.parent) then
+        parentatomic=sys.parent.is_part_of_atomic
+      end
+      if not(sys.is_part_of_acquired) and not(parentatomic) and not(sys.is_part_of_virtual) then
+        systosync << sys
+      end
+    }
+    systosync[10..20].each{|sys|
+      sync_issue(pr,sys,existing_issues)
+    }
+  end
+  
+  def create_issue(pr,ms)
+    if (not(ms.acquisition_workflow)) then
+      print("\n************ system without acquisition workflow: "+ms.system.to_s+"\n")
+    end
+    if (ms.acquisition_workflow) then
+      all_trackers=RedmineRest::Models::Tracker.all
+      tr=nil
+      all_trackers.each {|t|
+        print "Tracker #{t.name} item:"+t.id.to_s+"\n"
+        if t.name==ms.acquisition_workflow.name then
+          tr=t
+          print ("***** ESTE\n")
+        end
+      }
+
+      all_users=RedmineRest::Models::User.all
+      us=nil
+      all_users.each {|t|
+        print "User #{t.login} item:"+t.id.to_s+"\n"
+        if t.login=="txinto" then
+          us=t
+          print ("***** ESTE\n")
+        end
+      }  
+=begin
+      i.custom_fields << {:name => 'eosysid', :value => ms.id, :id => 11}
+=end      
+      print "Creamos una issue\n"
+      i=Issue.new
+      i.project=pr
+      i.tracker=tr
+      i.subject=tr.name+" of "+ms.to_s
+      i.author_id=us
+      i.status_id=1
+      i.priority_id=1
+      i.description="Issue to track " + tr.name+" of "+ms.to_s
+      i.custom_fields = []
+      i.custom_fields << {:name => 'eosys', :value => true, :id => 10}
+      i.custom_fields << {:name => 'eosysid', :value => "1", :id => 11}
+      print i.custom_fields.to_s
+      done=i.save
+      print "Hecho: "+done.to_s+"\n"
+      print "Issue:"+i.to_s+"\n"
+      print "Tracker:"+i.tracker.id.to_s+"\n"
+      print "Project:"+i.project.id.to_s+"\n"
+      # Creamos las sub issues
+      if (ms.acquisition_workflow.name=="Fabrication") then
+        # Create the spec
+        i2=Issue.new
+        i2.project=pr
+        i2.tracker=tr
+        i2.parent=i
+        i2.subject="Specification"+" of "+ms.to_s
+        i2.author_id=us
+        i2.status_id=1
+        i2.priority_id=1
+        i2.description="Issue to track " + "specification" + " of "+ms.to_s
+        i2.custom_fields = []
+        i2.custom_fields << {:name => 'eosys', :value => true, :id => 10}
+        i2.custom_fields << {:name => 'eosysid', :value => "1", :id => 11}
+        i2.estimated_hours=8
+        print i2.custom_fields.to_s
+        done=i2.save
+        print "Hecho: "+done.to_s+"\n"
+        print "Issue:"+i2.to_s+"\n"
+        print "Tracker:"+i2.tracker.id.to_s+"\n"
+        print "Project:"+i2.project.id.to_s+"\n"        
+
+        # Create the validation plan
+        i3=Issue.new
+        i3.project=pr
+        i3.tracker=tr
+        i3.parent=i
+        #i3.blocked_by << i2
+        i3.subject="Validation Plan"+" of "+ms.to_s
+        i3.author_id=us
+        i3.status_id=1
+        i3.priority_id=1
+        i3.description="Issue to track " + "validation plan" + " of "+ms.to_s
+        i3.custom_fields = []
+        i3.custom_fields << {:name => 'eosys', :value => true, :id => 10}
+        i3.custom_fields << {:name => 'eosysid', :value => "1", :id => 11}
+        i3.estimated_hours=8
+        print i3.custom_fields.to_s
+        done=i3.save
+        print "Hecho: "+done.to_s+"\n"
+        print "Issue:"+i3.to_s+"\n"
+        print "Tracker:"+i3.tracker.id.to_s+"\n"
+        print "Project:"+i3.project.id.to_s+"\n"        
+        r1=Relation.new
+        r1.issue_id=i2.id
+        r1.issue_to_id=i3.id
+        r1.relation_type="blocks"
+        r1.prefix_options[:issue_id] = i2.id
+        done=r1.save
+        print "Relation Hecho: "+done.to_s+"\n"
+
+        # Create the Design
+        i4=Issue.new
+        i4.project=pr
+        i4.tracker=tr
+        i4.parent=i
+        #i4.preceded_by << i3
+        i4.subject="Design"+" of "+ms.to_s
+        i4.author_id=us
+        i4.status_id=1
+        i4.priority_id=1
+        i4.description="Issue to track " + "design" + " of "+ms.to_s
+        i4.custom_fields = []
+        i4.custom_fields << {:name => 'eosys', :value => true, :id => 10}
+        i4.custom_fields << {:name => 'eosysid', :value => "1", :id => 11}
+        i4.estimated_hours=8
+        print i4.custom_fields.to_s
+        done=i4.save
+        print "Hecho: "+done.to_s+"\n"
+        print "Issue:"+i4.to_s+"\n"
+        print "Tracker:"+i4.tracker.id.to_s+"\n"
+        print "Project:"+i4.project.id.to_s+"\n"
+        r2=Relation.new
+        r2.issue_id=i3.id
+        r2.issue_to_id=i4.id
+        r2.relation_type="precedes"
+        r2.delay=0
+        r2.prefix_options[:issue_id] = i3.id
+        done=r2.save
+        print "Relation Hecho: "+done.to_s+"\n"
+      end
+    end 
+  end
+    
+  def sync_issue(pr,sys,exiss)
+    found=false
+    # Find correct tracker
+    sys.mech_systems.each{ |ms|
+      exiss.each{ |i|
+        if (i.system==ms.system) then
+          # TODO: ensure that issues rms deals with several mech_systems
+          # for the same system
+          exiss.delete(i)
+          found=true
+        end
+      }
+      if (not(found)) then
+        # The issue workflow has to be created
+        self.create_issue(pr,ms)
+      end
+    }
+  end
+
   # --- Permissions --- #
 
   def create_permitted?
